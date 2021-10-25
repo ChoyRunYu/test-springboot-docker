b2312a34a4322892eed57585542861f56cb343da
cairunyu <1063978024@qq.com>
2021-10-25 23:19:47 +0800

    初始化

---
+++ src/main/java/com/example/testspringbootdocker/TestSpringbootDockerApplication.java
@@ -0,0 +1,13 @@
+package com.example.testspringbootdocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringbootDockerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringbootDockerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testspringbootdocker/TestSpringbootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testspringbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringbootDockerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
